9b7863342087ea37d7ccd80a5d81ea5544a74da9
Manuchehr <manuchekhr.3232@gmail.com>
2025-02-13 10:34:35 +0500

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Eskiz SMS Nodejs (Typescript) package
+
+## Installation
+
+```bash
+pnpm add node-eskiz-sms
+```
+
+## License
+
+ISC
---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+// @ts-check
+import eslint from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+
+export default tseslint.config(
+  {
+    ignores: ["eslint.config.mjs"],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-floating-promises": "warn",
+      "@typescript-eslint/no-unsafe-argument": "warn",
+    },
+  },
+);
